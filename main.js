c3244f579203ea617b82d6511c13da8bcc4f8de9
jamshid1666 <saribayev94@mail.ru>
2025-04-10 11:34:28 +0500

    update main js

---
+++ main.js
@@ -0,0 +1,20 @@
+// const a = parseInt(prompt('yilni kiriting'));
+// const b = 2025 - a;
+// if (b >= 18) {
+//     alert('adult' )
+// } else {
+//     alert('minor')
+// }
+
+// const a = parseInt(prompt('son kiritng'));
+// if (a < 0 || a >= 100) {
+//     alert('100 gacha son kiriting')
+// } if(a % 3 === 0 && a % 5 === 0) {
+//     alert ('FIZZBUZZ')
+//  } else if (a % 3 === 0 ) {
+//     alert ('FIZZ')
+// } else if (a % 5 === 0) {
+//     alert('BUZZ') 
+// } else {
+//     alert(a)
+// }
